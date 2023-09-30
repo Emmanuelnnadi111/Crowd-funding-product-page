95a0bb6ddfe3231d2d736d58d4d1bbb42733ccea
Emmanuelnnadi111 <doneonyi920@gmail.com>
2023-09-30 19:55:11 +0100

    added backdrop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const openMenu = document.querySelector(".openBtn");
 const closeMenu = document.querySelector(".close-menu");
-const backToProjectBtn = document.querySelector(".back-to-project");
-const backToProjectPage = document.querySelector(".back-to-project-wrap ");
+const backToProjectBtn = document.querySelector(".back-to-project1");
+const backToProjectPage = document.querySelector(".back-to-project-wrap");
 const activeBtn = document.querySelector(".active-btn");
 const activeWrap = document.querySelector(".active-wrap");
 const continueBtn = document.querySelector(".continueBtn");
@@ -15,41 +15,44 @@ const closeBackdrop = document.querySelector(".cover-modal");
 const backtoprojectModal = document.querySelector(".backtoprojet-modal");
 
 openMenu.addEventListener("click", () => {
-	mobileMenu.classList.remove("hidden");
-	closeMenu.classList.remove("hidden");
-    openMenu.classList.add("hidden");
+  mobileMenu.classList.remove("hidden");
+  closeMenu.classList.remove("hidden");
+  openMenu.classList.add("hidden");
 });
 
 closeMenu.addEventListener("click", () => {
-    mobileMenu.classList.add("hidden");
-    openMenu.classList.remove("hidden");
-    closeMenu.classList.add("hidden");
+  mobileMenu.classList.add("hidden");
+  openMenu.classList.remove("hidden");
+  closeMenu.classList.add("hidden");
 });
 
 backToProjectBtn.addEventListener("click", (e) => {
-    backtoprojectModal.classList.remove("hidden");
-    e.preventDefault()
+  const display = document.querySelector(".back-to-project-wrap");
+  display.style.display = "block";
+  backtoprojectModal.style.display = 'block';
+  e.preventDefault();
 });
 
 closeModal.addEventListener("click", () => {
-    const display = document.querySelector(".back-to-project-wrap ");
-    display.style.display = "none";
-    backtoprojectModal.style.display = 'none';
+  const display = document.querySelector(".back-to-project-wrap");
+  display.style.display = "none";
+  backtoprojectModal.style.display = 'none';
+  backtoprojectModal.classList.add("hidden");
 });
 
 selectInput.addEventListener("click", () => {
-    document.querySelector(".bamboo-content").style.borderColor = "DarkCyan";
-    activeWrap.classList.remove("hidden");
-    selectInput.style.color = `" text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 "`;
+  document.getElementsByClassName("bamboo-content")[0].style.borderColor = "DarkCyan";
+  activeWrap.classList.remove("hidden");
+  selectInput.style.color = `" text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 "`;
 });
 
 continueBtn.addEventListener("click", () => {
-    closeBackdrop.classList.remove("hidden");
-     document.querySelector(".back-to-project-wrap").style.display = "none";
+  closeBackdrop.classList.remove("hidden");
+  document.getElementsByClassName("back-to-project-wrap")[0].style.display = "none";
 });
 
 completeBtn.addEventListener("click", () => {
-    complete.classList.add('hidden');
-    closeBackdrop.classList.add('hidden');
-    backtoprojectModal.style.display = "none";
+  complete.classList.add('hidden');
+  closeBackdrop.classList.add('hidden');
+  backtoprojectModal.style.display = "none";
 });
